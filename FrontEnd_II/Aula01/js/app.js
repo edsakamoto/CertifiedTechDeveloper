929e56406ec1181e25485bc537b2e5842436671c
edsakamoto <42415029+edsakamoto@users.noreply.github.com>
2022-02-21 22:20:07 -0300

    modificacoes no .js

--- FrontEnd_II/Aula01/js/app.js
+++ FrontEnd_II/Aula01/js/app.js
@@ -1,30 +1,7 @@
-//alert('ola visitante')
-
-//let resultado = confirm('deseja realmente excluir este cliente?');
-//console.log(resultado);
-// let edad = parseInt(prompt("Introduza sua idade"));
-
-// if(isNaN(edad)){
-
-//     console.log('Valor digitado não é um número. Favor informar um número')    
-
-// } else {if(edad>18){
-//     console.log("É maior de idade");         
-//     } else {console.log("É menor de idade");}    
-// };
-// let edad = ''
-// do {
-//     edad = parseInt(prompt("Introduza sua idade"));
-    
-// } while (edad != true);
-// if(edad>18){ 
-//     console.log("É maior de idade");         
-//     }else{ 
-//     console.log("É menor de idade"); 
-//     }    
-
 let arr = [1,2,4,8];
+
 let num = 0;
+
 for (var i = 0; i<arr.length; i++){
     num += arr[i];
 }
