f9d8768f8e2666ac48ec9a764ccac546948e5061
edsakamoto <42415029+edsakamoto@users.noreply.github.com>
2022-02-23 18:35:12 -0300

    corrigido problema de nao reconhecer o vencedor

--- FrontEnd_II/Aula02/js/app.js
+++ FrontEnd_II/Aula02/js/app.js
@@ -9,94 +9,75 @@ Guilherme Mello
 Lucas Bernardo
 
 */
-
-// function getRandomIntInclusive(min,max){
-//     min = Math.ceil(min);
-//     max = Math.floor(max);
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
-
-// let escolhaBot = getRandomIntInclusive(1,3);
-let escolhaBot = parseInt(Math.random()*3+1);
-
+ 
 const mensagem = ['Empatou','Robô ganhou','Você ganhou!'];
-
+                   /*0     ,    1        ,      2*/
 let pontosUsuario = 0;
 let pontosBot = 0;
+let semVencedor = true;
+let vencedor = ''
 
 do {
-let escolhaUsuario = parseInt(prompt('Digite um número: \n [1] - Pedra \n [2] - Papel \n [3] - Tesoura'));
+    let escolhaUsuario = parseInt(prompt('Digite um número: \n [1] - Pedra \n [2] - Papel \n [3] - Tesoura'));
 
-if(escolhaUsuario > 0 && escolhaUsuario < 4){
-    switch(escolhaUsuario){    
-        case 1:
-            switch(escolhaBot){
-                case 1:                    
-                    alert(mensagem[0]);
-                    break;
-                case 2:
-                    alert(mensagem[1]);
-                    pontosBot ++;
-                    break;
-                case 3:
-                    alert(mensagem[2]);
-                    pontosUsuario ++;
-                    break;
-            } break;
-        case 2:
-            switch(escolhaBot){                            
-                case 1:
-                    alert(mensagem[2]);
-                    pontosUsuario ++;
-                    break;
-                case 2:
-                    alert(mensagem[0]);
-                    break;
-                case 3:
-                    alert(mensagem[1]);
-                    pontosBot ++;
-                    break;
-            } break;
-        case 3:
-            switch(escolhaBot){
-                case 1:
-                    alert(mensagem[1]);
-                    pontosBot ++;
-                    break;
-                case 2:
-                    alert(mensagem[2]);
-                    pontosUsuario ++;
-                    break;
-                case 3:
-                    alert(mensagem[0]);
-                    break;
-            } break;
-        }
-    }
-}
-while (pontosUsuario < 3 || pontosBot < 3);
-console.log(pontosUsuario, pontosBot);
-/* for of */
-// const options = [1,2,3]
+    let escolhaBot = parseInt(Math.random()*3+1);
 
-// console.log(options);
-
-// for(let option of options){
-
-//     console.log(option);
+    if(escolhaUsuario > 0 && escolhaUsuario < 4){        
+        switch(escolhaUsuario){
+            case 1:                
+                switch(escolhaBot){                                        
+                    case 1:
+                        alert(mensagem[0]);
+                        break;
+                    case 2:
+                        alert(mensagem[1]);
+                        pontosBot ++;                    
+                        break;
+                    case 3:
+                        alert(mensagem[2]);
+                        pontosUsuario ++;
+                        break;
+                } 
+                break;
+            case 2:
+                switch(escolhaBot){                            
+                    case 1:
+                        alert(mensagem[2]);
+                        pontosUsuario ++;
+                        break;
+                    case 2:
+                        alert(mensagem[0]);
+                        break;
+                    case 3:
+                        alert(mensagem[1]);
+                        pontosBot ++;
+                        break;
+                    }
+                break;
+            case 3:
+                switch(escolhaBot){
+                    case 1:
+                        alert(mensagem[1]);
+                        pontosBot ++;
+                        break;
+                    case 2:
+                        alert(mensagem[2]);
+                        pontosUsuario ++;
+                        break;
+                    case 3:
+                        alert(mensagem[0]);
+                        break;
+                    } 
+                break;
+            }
+        } else { alert('jogue um valor válido')};
     
-// };
+   if(pontosBot == 2 || pontosUsuario == 2){
+       semVencedor = false;
+       if(pontosBot == 2){
+           vencedor = 'Robo ganhou 2x';
+       } else{ vencedor = 'Jogador ganhou 2x'};
+   }
+} while (semVencedor);
 
-/* for in */
-
-// const options = {
-//     opcao: [1,2,3],
-//     descricao: ['PE','PA','TE']
-// }
-
-// // console.log(options.opcao[0]);
-
-// for(let choosedOption in options){
-//     console.log(options[choosedOption]);
-// }
+alert(vencedor);
